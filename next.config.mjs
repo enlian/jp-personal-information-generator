1bc3d5aad35077a8d4c88b4e1d80541eadad39ad
enlian <frejaenlian@gmail.com>
2024-10-06 00:48:00 +0900

    fix bug

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import CopyWebpackPlugin from 'copy-webpack-plugin';
+import path from 'path';
+
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins.push(
+        new CopyWebpackPlugin({
+          patterns: [
+            {
+              from: path.resolve('node_modules/kuromoji/dict'),
+              to: path.resolve('public'),
+            },
+          ],
+        })
+      );
+    }
+    return config;
+  },
+};
 
 export default nextConfig;
